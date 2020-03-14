88fcaa3a62aeda75f2f69e5b8423b32ad38e820c
Charles Meng <mengcharles@gmail.com>
2020-03-15 00:17:35 +0800

    initial upload

---
+++ getcommand.ps1
@@ -0,0 +1,10 @@
+$loc = Get-Location
+$loc = $loc.Path + "\command.bat"
+try {
+    $client_process = Get-CimInstance Win32_Process -Filter "name = 'LeagueClient.exe'"
+    Stop-Process $client_process.ProcessId
+    $command = $client_process.CommandLine
+    $command -replace("zh_TW", "en_US") | Out-File -FilePath "$loc" -Encoding ascii
+}
+catch {}
+finally {& $loc}
